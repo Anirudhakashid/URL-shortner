7609721a535dc159b03b3a6723365d16550dfde3
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-06-02 15:37:54 +0530

    landing page setup

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function LandingPage() {
-  return <div>LandingPage</div>;
+  return (
+    <div>
+      <h2 className=" my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl text-center text-slate-100 font-extrabold ">
+        Shorten. Share. Track. Simplify your links in seconds..
+      </h2>
+    </div>
+  );
 }
 
 export default LandingPage;
