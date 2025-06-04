395e50420c5fad50baed747a76b59ffb32a7d0de
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-06-04 18:11:28 +0530

    Implement login form with validation using Yup and error handling

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { BeatLoader } from "react-spinners";
+import Error from "./Error";
+import * as yup from "yup";
 
 function Login() {
-  return <div>Login</div>;
+  const [errors, setErrors] = useState([]);
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  //Validation logic using Yup
+  const handleLogin = async () => {
+    setErrors([]);
+    try {
+      const schema = yup.object().shape({
+        email: yup
+          .string()
+          .email("Invalid email")
+          .required("Email is required"),
+        password: yup
+          .string()
+          .min(6, "Password must be at least 6 characters")
+          .required("Password is required"),
+      });
+
+      // Validate the form data against the schema
+      await schema.validate(formData, { abortEarly: false });
+      // abortEarly: false is useful when you want to show all form errors at once to the user.
+    } catch (e) {
+      const newErrors = {};
+
+      e?.inner?.forEach((err) => {
+        newErrors[err.path] = err.message; // Collect all errors by their path
+      });
+      setErrors(newErrors);
+    }
+  };
+
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+          <Error message="some thing went wrong" />
+        </CardHeader>
+        <CardContent className="space-y-2">
+          <div className="space-y-0.5">
+            <Input
+              name="email"
+              type="email"
+              placeholder="Enter Email"
+              onChange={handleInputChange}
+            />
+            {errors.email && <Error message={errors.email} />}
+          </div>
+          <div className="space-y-1">
+            <Input
+              name="password"
+              type="password"
+              placeholder="Enter Password"
+              onChange={handleInputChange}
+            />
+            {errors.email && <Error message={errors.password} />}
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button onClick={handleLogin}>
+            {true ? <BeatLoader size={5} /> : "Login"}
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
 }
 
 export default Login;
