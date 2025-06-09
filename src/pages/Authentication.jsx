d1ab2530bd28bb0e022086288bb173a2e2fc7234
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-06-09 14:14:25 +0530

    Refactored to include navigation and authentication logic

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -1,19 +1,28 @@
-import React from "react";
-import { useSearchParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Login from "@/Components/Login";
 import Signup from "@/Components/Signup";
+import { UrlState } from "@/Context";
 
 function Authentication() {
   const [searchParam] = useSearchParams();
+  const longLink = searchParam.get("createNew");
+  const navigate = useNavigate();
+
+  const { loading, isAuthenticated } = UrlState();
+
+  useEffect(() => {
+    if (isAuthenticated && !loading) {
+      navigate(`/dashboard?${longLink ? `createNew=${longLink}` : ""}`);
+    }
+  }, [isAuthenticated, loading]);
 
   return (
     <>
       <div className="mt-28 flex flex-col items-center gap-10">
         <h1 className="text-5xl font-extrabold">
-          {searchParam.get("createNew")
-            ? "Kindly log in to continue!"
-            : "Login / Signup"}
+          {longLink ? "Kindly log in to continue!" : "Login / Signup"}
         </h1>
         <Tabs defaultValue="Login" className="w-[400px]">
           <TabsList className={"grid w-full grid-cols-2"}>
