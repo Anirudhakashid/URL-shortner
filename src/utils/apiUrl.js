127bc9030021f591445d376ece095def1547c34f
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-06-12 17:52:33 +0530

    Added createUrl function

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getUrls(user_id) {
   const { data, error } = await supabase
@@ -20,6 +20,46 @@ export async function deleteUrl(id) {
   if (error) {
     console.error(error.message);
     throw new Error("Failed to fetch URLs");
+  }
+
+  return data;
+}
+
+export async function createUrl(
+  { title, longUrl, customUrl, user_id },
+  qrcode
+) {
+  //* generating a short URL
+  const short_url = Math.random().toString(36).substring(2, 6);
+
+  //*Logic for uploding qrcode
+  const fileName = `qr-${short_url}`;
+  const { error: storageError } = await supabase.storage
+    .from("qrs")
+    .upload(fileName, qrcode);
+
+  if (storageError) throw new Error(storageError.message);
+
+  const qr = `${supabaseUrl}/storage/v1/objects/public/qrs/${fileName}`;
+
+  //* creating the url
+  const { data, error } = await supabase
+    .from("urls")
+    .insert([
+      {
+        title,
+        original_url: longUrl,
+        short_url,
+        custom_url: customUrl,
+        user_id,
+        qr_code: qr,
+      },
+    ])
+    .select();
+
+  if (error) {
+    console.error(error.message);
+    throw new Error("Error creating short URL");
   }
 
   return data;
