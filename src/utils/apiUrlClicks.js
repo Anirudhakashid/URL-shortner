dbf399feb0e3e998ccec158d3d04fd1f37c25fac
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-06-19 15:54:33 +0530

    Fixed device type issue

--- src/utils/apiUrlClicks.js
+++ src/utils/apiUrlClicks.js
@@ -21,7 +21,7 @@ const parser = new UAParser(); // detects browser and device type
 export const storeClicks = async ({ id, originalUrl }) => {
   try {
     const res = parser.getResult();
-    const device = res.type || "desktop";
+    const device = res?.device.type || "desktop";
 
     const response = await fetch("https://ipapi.co/json");
     const { city, country_name: country } = await response.json();
